a55e5975ff3d87d69a25065a0969a04a33ababf2
johnny <johnminor07@gmail.com>
2016-01-23 19:36:36 -0800

    more variables and printing

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Johnny Minor'
+my_age = 26  # age is just a number anyways.
+my_height = 71  # inches
+my_weight = 190  # pounds
+my_eyes = 'Brown'
+my_teeth = 'whiteish'
+my_hair = 'Black'
+
+print "Lets talk about %s." % name
+print "He's %d inches tall." % my_age
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy..."
+print "He's got %s eyes and %s color hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on how much coffee and tea." % my_teeth
+
+# I'll try to get this right the first time.
+print "If I add %d, %d, and %d then I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
